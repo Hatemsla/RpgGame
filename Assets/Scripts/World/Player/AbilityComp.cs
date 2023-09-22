d596cbc8389ae782622545eacbe041b99b30d638
Manfred246 <mbeldiy@mail.ru>
2023-09-22 12:41:04 +0300

    Added ability component

---
+++ Assets/Scripts/World/Player/AbilityComp.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace World.Player
+{
+    [CreateAssetMenu]
+    public class AbilityComp : ScriptableObject
+    {
+        public string Name;
+        public float CostPoint;
+        public float Damage;
+        public float Distance;
+    }
+}

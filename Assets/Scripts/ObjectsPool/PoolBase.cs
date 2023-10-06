c088cef93741b44440ac3fcce6e9f17d8abe5cf5
Manfred246 <mbeldiy@mail.ru>
2023-10-06 12:25:28 +0300

    Added mana mana spending and recovery system
    Added pool base

---
+++ Assets/Scripts/ObjectsPool/PoolBase.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utils.ObjectsPool
+{
+    public class PoolBase<T>
+    {
+        #region fields
+
+        private readonly Func<T> _preloadFunc;
+        private readonly Action<T> _getAction;
+        private readonly Action<T> _returnAction;
+
+        private Queue<T> _pool = new();
+        private readonly List<T> _active = new();
+
+        #endregion
+
+        #region constructor
+
+        public PoolBase(Func<T> preloadFunc, Action<T> getAction, Action<T> returnAction, int preloadCount)
+        {
+            _preloadFunc = preloadFunc;
+            _getAction = getAction;
+            _returnAction = returnAction;
+
+            if (preloadFunc == null)
+            {
+                Debug.LogError("Preload func is null");
+                return;
+            }
+
+            for (var i = 0; i < preloadCount; i++)
+                Return(preloadFunc());
+        }
+
+        #endregion
+
+        #region methods
+
+        public T Get()
+        {
+            var item = _pool.Count > 0 ? _pool.Dequeue() : _preloadFunc();
+            _getAction(item);
+            _active.Add(item);
+
+            return item;
+        }
+
+        public void Return(T item)
+        {
+            _returnAction(item);
+            _pool.Enqueue(item);
+            _active.Remove(item);
+        }
+
+        public bool HasItem(T item)
+        {
+            return _active.Contains(item);
+        }
+
+        public void ReturnAll()
+        {
+            foreach (var item in _active.ToArray()) Return(item);
+        }
+
+        #endregion
+    }
+}
--- Assets/Scripts/World/Player/PlayerSpellCastSystem.cs
+++ Assets/Scripts/World/Player/PlayerSpellCastSystem.cs
@@ -10,32 +10,36 @@ namespace World.Player
 {
     public sealed class PlayerSpellCastSystem : IEcsRunSystem
     {
-        private readonly EcsFilterInject<Inc<PlayerComp, PlayerInputComp, RpgComp>> _playerMove = default;
+        private readonly EcsFilterInject<Inc<PlayerComp, PlayerInputComp, RpgComp>> _player = default;
         private readonly EcsCustomInject<Configuration> _cf = default;
         private readonly EcsCustomInject<SceneData> _sd = default;
 
         public void Run(IEcsSystems systems)
         {
-            foreach (var entity in _playerMove.Value)
+            foreach (var entity in _player.Value)
             {
-                ref var player = ref _playerMove.Pools.Inc1.Get(entity);
-                ref var input = ref _playerMove.Pools.Inc2.Get(entity);
-                ref var rpg = ref _playerMove.Pools.Inc3.Get(entity);
+                ref var player = ref _player.Pools.Inc1.Get(entity);
+                ref var input = ref _player.Pools.Inc2.Get(entity);
+                ref var rpg = ref _player.Pools.Inc3.Get(entity);
 
                 if(rpg.IsDead) return;
                 
                 if (input.UseAbility)
                 {
                     var spellObjectPrefab = _cf.Value.abilityConfiguration.abilityDatas[0];
-                    Debug.Log(_sd.Value.mainCamera);
-                    var spellDirection = 
-                        _sd.Value.mainCamera.OutputCamera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
-                    var spellObject = 
-                        Object.Instantiate(spellObjectPrefab.spell, player.Transform.position + player.Transform.forward, 
-                            Quaternion.identity);
-                    spellObject.spellTime = spellObjectPrefab.lifeTime;
-                    spellObject.spellSpeed = spellObjectPrefab.speed;
-                    spellObject.spellDirection = spellDirection.direction;
+
+                    if (rpg.Mana >= spellObjectPrefab.costPoint)
+                    {
+                        rpg.Mana -= spellObjectPrefab.costPoint;
+                        var spellDirection = 
+                            _sd.Value.mainCamera.OutputCamera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+                        var spellObject = 
+                            Object.Instantiate(spellObjectPrefab.spell, player.Transform.position + player.Transform.forward, 
+                                Quaternion.identity);
+                        spellObject.spellTime = spellObjectPrefab.lifeTime;
+                        spellObject.spellSpeed = spellObjectPrefab.speed;
+                        spellObject.spellDirection = spellDirection.direction;
+                    }
                 }
                 
             }
